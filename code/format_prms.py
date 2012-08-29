94ac50a0c69a229a0ec2e115eaa89aa939f9c325
Bart Baker <bartbkr@gmail.com>
2012-08-28 21:53:33 -0400

    Still working on output to latex tabular from numpy arrays

--- code/format_prms.py
+++ code/format_prms.py
@@ -1,30 +1,42 @@
 # coding: utf-8
 import pickle
+import re
 import pandas as px
 import numpy as np
 
 #!!!need to get easier output into table
 
+res_loc = "../results/"
+
 #get lambda_0 estimates
-pkl_file = open("collect_0_ref_curve.pkl", "rb")
+pkl_file = open(res_loc + "collect_0_ref_curve.pkl", "rb")
 collect_0_ref = pickle.load(pkl_file)
 print "lambda_0 parameter estimates"
 print collect_0_ref[3][0]
-print(np.round(collect_0_ref[3][1], decimals=4))
-for row in np.size(collect_0_ref[3][1], axis=0):
+out_str = str(np.round(collect_0_ref[3][1], decimals=4))
+out_str = re.sub("]", " &", out_str)
+out_str = re.sub("\[+", " ", out_str)
+print out_str
+#for row in np.size(collect_0_ref[3][1], axis=0):
+    #for col in np.size(collect_0_ref[3][1], axis=1):
+        #print collect_0_ref[3][1][row,col] 
 
 pkl_file.close()
 
 #get lamda_1 estiamtes
-pkl_file = open("collect_1_ref_curve.pkl", "rb")
+pkl_file = open(res_loc + "collect_1_ref_curve.pkl", "rb")
 collect_1_ref = pickle.load(pkl_file)
 print "lambda_1 parameter estimates"
 print collect_1_ref[3][0]
-print(np.round(collect_1_ref[3][1][:5, :5], decimals=4))
+out_str = str(np.round(collect_1_ref[3][1][:5, :5], decimals=4))
+out_str = re.sub("]", "", out_str)
+out_str = re.sub("\[+", " ", out_str)
+out_str = re.sub(" +", " &", out_str)
+print out_str
 pkl_file.close()
 
 #get var-cov matrix
-pkl_file = open("collect_cov_ref.pkl", "rb")
+pkl_file = open(res_loc + "collect_cov_ref.pkl", "rb")
 cov = pickle.load(pkl_file)
 print cov[3][0]
 #variance
