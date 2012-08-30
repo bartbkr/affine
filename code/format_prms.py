f2cd5e697652d638530c9b9fb20b93f804e2a52a
Bart Baker <bartbkr@gmail.com>
2012-08-29 20:15:45 -0400

    Finished formatting and inserting params from python into tex

--- code/format_prms.py
+++ code/format_prms.py
@@ -12,25 +12,23 @@ res_loc = "../results/"
 pkl_file = open(res_loc + "collect_0_ref_curve.pkl", "rb")
 collect_0_ref = pickle.load(pkl_file)
 print "lambda_0 parameter estimates"
-print collect_0_ref[3][0]
+#print collect_0_ref[3][0]
 out_str = str(np.round(collect_0_ref[3][1], decimals=4))
 out_str = re.sub("]", " &", out_str)
 out_str = re.sub("\[+", " ", out_str)
+out_str = re.sub("\n", "\n&\n", out_str)
 print out_str
-#for row in np.size(collect_0_ref[3][1], axis=0):
-    #for col in np.size(collect_0_ref[3][1], axis=1):
-        #print collect_0_ref[3][1][row,col] 
-
 pkl_file.close()
 
 #get lamda_1 estiamtes
 pkl_file = open(res_loc + "collect_1_ref_curve.pkl", "rb")
 collect_1_ref = pickle.load(pkl_file)
 print "lambda_1 parameter estimates"
-print collect_1_ref[3][0]
+#print collect_1_ref[3][0]
+#print np.round(collect_1_ref[3][1][:5, :5], decimals=4)
 out_str = str(np.round(collect_1_ref[3][1][:5, :5], decimals=4))
 out_str = re.sub("]", "", out_str)
-out_str = re.sub("\[+", " ", out_str)
+out_str = re.sub("\[+", "", out_str)
 out_str = re.sub(" +", " &", out_str)
 print out_str
 pkl_file.close()
@@ -40,9 +38,14 @@ pkl_file = open(res_loc + "collect_cov_ref.pkl", "rb")
 cov = pickle.load(pkl_file)
 print cov[3][0]
 #variance
-print "parameter est variance"
-print np.round(np.diag(cov[3][1]), decimals=4)
+#print "parameter est variance"
+#print np.round(np.diag(cov[3][1]), decimals=4)
 #std err
 print "parameter est std err"
-print np.round(np.sqrt(np.diag(cov[3][1])), decimals=4)
+#print np.round(np.sqrt(np.diag(cov[3][1])), decimals=4)
+out_str = str(np.round(np.sqrt(np.diag(cov[3][1])), decimals=4))
+out_str = re.sub("]", "", out_str)
+out_str = re.sub("\[+", "", out_str)
+out_str = re.sub(" +", " &", out_str)
+print out_str
 pkl_file.close()
