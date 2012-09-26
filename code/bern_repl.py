6c377c02c5184abc01642b1d787784ebdf034ba3
Bart Baker <bartbkr@gmail.com>
2012-09-26 09:51:52 -0400

    Began work on replicating BSR model

---
+++ code/bern_repl.py
@@ -0,0 +1,38 @@
+"""
+This script attempts to replicate the Bernanke, Sack, and Reinhart (2004) model
+"""
+import numpy as np
+import pandas as px
+
+import socket
+import atexit
+import keyring
+
+from statsmodels.tsa.api import VAR
+from statsmodels.tsa.filters import hpfilter
+from scipy import stats
+from util import pickle_file, success_mail, fail_mail, to_mth, gen_guesses
+
+#identify computer
+#identify computer
+comp = socket.gethostname()
+
+########################################
+# Get macro data                       #
+########################################
+mthdata = px.read_csv("../data/VARbernankedata.csv", na_values="M",
+                        index_col = 0, parse_dates=True)
+
+mthdata['tr_empl_gap'], mthdata['hp_ch'] = \
+    hpfilter(mthdata['Total_Nonfarm_employment'].dropna(), lamb=129600)
+mthdata['tr_empl_gap_perc'] = mthdata['tr_empl_gap']/mthdata['hp_ch'] * 100
+mthdata['act_infl'] = \
+    mthdata['Pers_Cons_P'].diff(periods=12)/mthdata['Pers_Cons_P']*100
+mthdata['ed_fut'] = 100 - mthdata['one_year_ED']
+
+#define final data set
+mod_data = mthdata.reindex(columns=['tr_empl_gap_perc',
+                                   'act_infl',
+                                   'output_deflator_year_ahead',
+                                    'fed_funds',
+                                    'ed_fut']).dropna(axis=0)
