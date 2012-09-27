08603e89f0d19c22b898a9c87eb193ca30dda3ad
Bart Baker <bartbkr@gmail.com>
2012-09-26 21:33:22 -0400

    fixed bsr data frame creation

--- code/bern_repl.py
+++ code/bern_repl.py
@@ -23,8 +23,12 @@ comp = socket.gethostname()
 mthdata = px.read_csv("../data/VARbernankedata.csv", na_values="M",
                         index_col = 0, parse_dates=True)
 
-mthdata['tr_empl_gap'], mthdata['hp_ch'] = \
-    hpfilter(mthdata['Total_Nonfarm_employment'].dropna(), lamb=129600)
+nonfarm = mthdata['Total_Nonfarm_employment'].dropna()
+
+tr_empl_gap, hp_ch = hpfilter(nonfarm, lamb=129600)
+
+mthdata['tr_empl_gap'] = px.Series(tr_empl_gap, index=nonfarm.index)
+mthdata['hp_ch'] = px.Series(hp_ch, index=nonfarm.index)
 mthdata['tr_empl_gap_perc'] = mthdata['tr_empl_gap']/mthdata['hp_ch'] * 100
 mthdata['act_infl'] = \
     mthdata['Pers_Cons_P'].diff(periods=12)/mthdata['Pers_Cons_P']*100
