42689edcd94bc86e7fb17b14767536b9e618eb25
Bart Baker <bartbkr@gmail.com>
2012-10-07 20:36:03 -0400

    Tried different solution method, still debuggin

--- code/tp_proj_unob.py
+++ code/tp_proj_unob.py
@@ -98,7 +98,7 @@ lam_0_g, lam_1_g, delta_1_g, mu_g, phi_g, sigma_g = gen_guesses(k_ar=k_ar,
 #bsr_solve = bsr_model.solve(lam_0_g=lam_0_g, lam_1_g=lam_1_g, method="nls")
 bsr_solve = bsr_model.solve(lam_0_g=lam_0_g, lam_1_g=lam_1_g,
                             delta_1_g=delta_1_g, mu_g=mu_g, phi_g=phi_g,
-                            sigma_g=sigma_g, method="ml", alg="bfgs")
+                            sigma_g=sigma_g, method="ml", alg="nm")
 
 lam_0 = bsr_solve[0]
 lam_1 = bsr_solve[1]
