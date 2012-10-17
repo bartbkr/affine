e52e5a8ce470f652d7e94a18f300c076363a704b
Bart Baker <bartbkr@gmail.com>
2012-10-17 15:25:51 -0400

    Final graphs for field_sketch and more flexible starting parameter est

--- code/util.py
+++ code/util.py
@@ -48,7 +48,11 @@ def robust(mod_data, mod_yc_data, method=None, lam_0_g=None, lam_1_g=None,
     #test sum_sqr_pe
     if lam_0_g is None:
         lam_0_g = np.zeros([neqs * k_ar, 1])
-        lam_0_g[:neqs] = np.array([[-0.1], [0.1], [-0.1], [0.1], [-0.1]])
+        for eq in range(neqs):
+            if eq % 2 == 0:
+                lam_0_g[eq] = -0.1
+            else:
+                lam_0_g[eq] = 0.1
 
     #set seed for future repl
 
@@ -59,9 +63,8 @@ def robust(mod_data, mod_yc_data, method=None, lam_0_g=None, lam_1_g=None,
                 mult = 1
             else: 
                 mult = -1
-            guess = [[mult*-0.1], [mult*0.1], [mult*-0.1], [mult*0.1], \
-                     [mult*-0.1]]
-            lam_1_g[eqnumb, :neqs, None] = np.array([guess])*np.random.random()
+            guess = ([mult * -0.1, mult * 0.1] * neqs)[:neqs]
+            lam_1_g[eqnumb, :neqs, None] = np.array([guess]).T*np.random.random()
 
     outs = bsr.solve(lam_0_g=lam_0_g, lam_1_g=lam_1_g, method=method,
                         ftol=1e-950, xtol=1e-950, maxfev=1000000000,
