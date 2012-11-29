66e2fc7723c7c1bc081b3bf24c63ca908dcf0959
Bart Baker <bartbkr@gmail.com>
2012-11-29 17:44:40 -0500

    save figures instead of showing

--- code/est_error.py
+++ code/est_error.py
@@ -146,16 +146,16 @@ ten_yr_plot = ten_yr.reindex(columns = ['120_mth_act',
 fig = ten_yr_plot.plot(style=['-', '--', '-.'], legend=False)
 handles, old_labels = fig.get_legend_handles_labels()
 fig.legend(handles, ('Actual', 'Predicted', 'Risk-neutral'))
-#plt.savefig("../../diss_writeup/figures/tenyr_rep.png")
-plt.show()
+plt.savefig("../../diss_writeup/figures/tenyr_rep.png")
+#plt.show()
 #two year
 two_yr_plot = two_yr.reindex(columns = ['24_mth_act',
     '24_mth_pred', '24_mth_nrsk'])
 fig = two_yr_plot.plot(style=['-', '--', '-.'])
 handles, old_labels = fig.get_legend_handles_labels()
 fig.legend(handles, ('Actual', 'Predicted', 'Risk-neutral'))
-#plt.savefig("../../diss_writeup/figures/twoyr_rep.png")
-plt.show()
+plt.savefig("../../diss_writeup/figures/twoyr_rep.png")
+#plt.show()
 
 #generate st dev of residuals
 yields = ['six_mth', 'one_yr', 'two_yr', 'three_yr', 'five_yr', 'seven_yr', 
