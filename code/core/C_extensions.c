c5367f23aad15fc7118e74370b25a058de074794
Bart Baker <bartbkr@gmail.com>
2013-01-07 09:48:26 -0500

    Still debugging C extension

--- code/core/C_extensions.c
+++ code/core/C_extensions.c
@@ -46,7 +46,7 @@ void mat_subtract(int rows, int cols, double arr1[rows][cols],
 
 /*  ==== Matrix product functions ===== */
 void mat_prodct(int row1, int col1, double arr1[row1][col1], int row2, 
-                int col2, double **arr2[row2][col2], 
+                int col2, double arr2[row2][col2], 
                 double result[row1][col2]) {
 
     /* What about case when results in single number */
