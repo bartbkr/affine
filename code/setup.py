c32d0dbc891c70ac67e06f46bc3630ba358c5014
Bart Baker <bartbkr@gmail.com>
2013-01-07 21:07:12 -0500

    Working on setup script for installation

--- code/setup.py
+++ code/setup.py
@@ -1,6 +1,11 @@
 from distutils.core import setup, Extension
 
-module1 = Extension('C_extension.c')
+module1 = Extension('C_extension.c', ['C_extension.c'])
 
 setup(name = 'Barton Baker',
-      version = 'v1.0')
+      version = '1.0',
+      description = 'This package offers a complete solver class for affine ' \
+                    + 'models, specifically affine models of the term ' \
+                    + 'structure',
+     author_email = "bartbkr@gmail.com",
+     ext_modules = [module1])
