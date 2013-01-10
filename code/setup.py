74ba358ace9fde2c0762356a6ef75dea6b21c636
Bart Baker <bartbkr@gmail.com>
2013-01-09 21:34:53 -0500

    Working on setup.py install script

--- code/setup.py
+++ code/setup.py
@@ -1,9 +1,18 @@
 from distutils.core import setup, Extension
 
-module1 = Extension('C_extension.c', ['C_extension.c'])
+module1 = Extension('C_extension.c', ['affine/C_extension.c'])
+
+#Package requires numpy
+try:
+    import numpy as np
+except ImportError:
+    nonumpy_msg = ("# numpy needed to finish setup.  run:\n\n"
+    "    $ pip install numpy  # or easy_install numpy\n")
+    sys.exit(nonumpy_msg)
 
 setup(name = 'Barton Baker',
       version = '1.0',
+      include_dirs = [np.get_include()],
       description = 'This package offers a complete solver class for affine ' \
                     + 'models, specifically affine models of the term ' \
                     + 'structure',
