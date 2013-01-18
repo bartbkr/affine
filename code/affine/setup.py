52fa25adff8b92538e647fd1957c089ef8e5aa5e
Bart Baker <bartbkr@gmail.com>
2013-01-18 11:39:20 -0500

    Working on setup

--- code/affine/setup.py
+++ code/affine/setup.py
@@ -11,12 +11,13 @@ except ImportError:
 c_extentsion = Extension('_C_extensions', ['C_extensions.c'],
                     include_dirs=[np.get_include() + "/numpy"])
 
-setup(name = 'py_affine',
-      author = 'Barton Baker',
-      version = '1.0',
-      include_dirs = [np.get_include()],
-      description = 'This package offers a complete solver class for affine ' \
+setup(name='py_affine',
+      author='Barton Baker',
+      version='1.0',
+      packages=['affine']
+      include_dirs=[np.get_include()],
+      description='This package offers a complete solver class for affine ' \
                     + 'models, specifically affine models of the term ' \
                     + 'structure',
-     author_email = "bartbkr@gmail.com",
-     ext_modules = [c_extentsion])
+     author_email="bartbkr@gmail.com",
+     ext_modules=[c_extentsion])
