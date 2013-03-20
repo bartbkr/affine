10a822bd7e4006f780fde780beaa2ddbc8c69c3f
Bart Baker <bartbkr@gmail.com>
2013-03-19 20:24:50 -0400

    Fixed parameter gleaning

--- code/format_prms.py
+++ code/format_prms.py
@@ -7,8 +7,8 @@ import numpy as np
 neqs = 5
 
 #get lambda_0 estimates
-pkl_file = open("../results/bern_nls/lam_0_all_nls.pkl", "rb")
-collect_0_ref = pickle.load(pkl_file)[0]
+pkl_file = open("../results/bern_nls/collect_lam_0_ref_nls.pkl", "rb")
+collect_0_ref = pickle.load(pkl_file)[0][1][5]
 print "lambda_0 parameter estimates"
 #print collect_0_ref[3][0]
 np.set_printoptions(precision=4)
@@ -21,8 +21,8 @@ print out_str
 pkl_file.close()
 
 #get lamda_1 estiamtes
-pkl_file = open("../results/bern_nls/lam_1_all_nls.pkl", "rb")
-collect_1_ref = pickle.load(pkl_file)[0]
+pkl_file = open("../results/bern_nls/collect_lam_1_ref_nls.pkl", "rb")
+collect_1_ref = pickle.load(pkl_file)[0][1][5]
 print "lambda_1 parameter estimates"
 #print collect_1_ref[3][0]
 out_str = str(collect_1_ref[:neqs, :neqs])
@@ -33,8 +33,8 @@ print out_str
 pkl_file.close()
 
 #get var-cov matrix
-pkl_file = open("../results/bern_nls/cov_all_nls.pkl", "rb")
-cov = pickle.load(pkl_file)[0]
+pkl_file = open("../results/bern_nls/collect_cov_ref_nls.pkl", "rb")
+cov = pickle.load(pkl_file)[0][1][5]
 #print cov
 #variance
 #print "parameter est variance"
@@ -42,7 +42,7 @@ cov = pickle.load(pkl_file)[0]
 #std err
 print "parameter est std err"
 #print np.round(np.sqrt(np.diag(cov[3][1])), decimals=4)
-out_str = str(np.reshape(np.sqrt(np.diag(cov)), (neqs + 1, neqs)))
+out_str = str(np.reshape(np.sqrt(np.diag(cov)), (neqs + 1, neqs)).T)
 out_str = re.sub("]", "", out_str)
 out_str = re.sub("\[+", "", out_str)
 out_str = re.sub(" +", " &", out_str)
