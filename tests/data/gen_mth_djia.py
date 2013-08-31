908187836a825c3cfd2f97270e672cfeb950ca21
Bart Baker <bartbkr@gmail.com>
2013-08-31 13:53:06 -0400

    Updated script

--- tests/data/gen_mth_djia.py
+++ tests/data/gen_mth_djia.py
@@ -1,7 +1,9 @@
 # coding: utf-8
 import pandas as pa
+import numpy as np
 djia = pa.read_csv('./DJIA.csv', index_col='DATE', parse_dates=True)
 djia = djia.astype('object').convert_objects(convert_numeric=True)
+djia['logVAlUE'] = np.log(djia['VALUE'])
 by = lambda x: lambda y: getattr(y, x)
 djia_std_mth = djia.groupby([by('year'), by('month')]).std()
 print djia_std_mth.head(30)
