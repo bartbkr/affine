d8380bcdb00177cbeb19ce49eba1774cac3c2315
Bart Baker (AU HPC) <bartbkr@gmail.com>
2013-11-06 09:39:59 -0500

    Added runs

--- tests/execute_mse_orig.py
+++ tests/execute_mse_orig.py
@@ -3,7 +3,7 @@ This script attempts to replicate the Bernanke, Sack, and Reinhart (2004) model
 """
 import numpy as np
 import numpy.ma as ma
-import pandas as pa
+import pandas as px
 import datetime as dt
 import matplotlib.pyplot as plt
 
@@ -19,7 +19,16 @@ from affine.model.affine import Affine
 import ipdb
 
 xtols = [#0.1,
+         #0.05,
+         #0.03,
+         #0.01,
+         #0.009,
+         #0.005,
+         #0.001,
+         #0.0001,
+         #0.00001,
          0.000001,
+         #0.0000001,
         ]
 
 ftols = [1.49012e-8]
@@ -69,57 +78,25 @@ models = [['tr_empl_gap_perc',
            'disag',
            'vix_cboe']]
 
-model_names = ['b',
-               'b+E',
-               'b+D',
-               'b+V',
-               'b+V+D',
-               'b+E+D',
-               'b+E+V',
-               'b+E+D+V']
-
-sources = [#'orig', 
-           'fbliss']
-
-yc_dates = pa.date_range("8/1/1990", "5/1/2012", freq="MS").to_pydatetime()
-
-dfs = ['one_year_res_orig',
-       'two_year_res_orig',
-       'three_year_res_orig',
-       'five_year_res_orig',
-       'one_year_res_fbliss',
-       'two_year_res_fbliss',
-       'three_year_res_fbliss',
-       'five_year_res_fbliss']
-
-#set up data frames to hold results
-one_year_res_orig = pa.DataFrame(index=yc_dates)
-two_year_res_orig = pa.DataFrame(index=yc_dates)
-three_year_res_orig = pa.DataFrame(index=yc_dates)
-five_year_res_orig = pa.DataFrame(index=yc_dates)
-
-one_year_res_fbliss = pa.DataFrame(index=yc_dates)
-two_year_res_fbliss = pa.DataFrame(index=yc_dates)
-three_year_res_fbliss = pa.DataFrame(index=yc_dates)
-five_year_res_fbliss = pa.DataFrame(index=yc_dates)
+sources = ['orig']
 
 for source in sources:
-    for mix, model in enumerate(models):
+    for model in models:
         print "================================="
         for xtol in xtols:
             for ftol in ftols:
                 ########################################
                 # Get macro data                       #
                 ########################################
-                mthdata = pa.read_csv("./data/macro_data.csv", na_values="M",
-                                        index_col = 0, parse_dates=True,
+                mthdata = px.read_csv("./data/macro_data.csv", na_values="M",
+                                        index_col = 0, parse_dates=True, 
                                         sep=";")
                 nonfarm = mthdata['Total_Nonfarm_employment_seas'].dropna()
 
                 tr_empl_gap, hp_ch = hpfilter(nonfarm, lamb=129600)
 
-                mthdata['tr_empl_gap'] = pa.Series(tr_empl_gap, index=nonfarm.index)
-                mthdata['hp_ch'] = pa.Series(hp_ch, index=nonfarm.index)
+                mthdata['tr_empl_gap'] = px.Series(tr_empl_gap, index=nonfarm.index)
+                mthdata['hp_ch'] = px.Series(hp_ch, index=nonfarm.index)
                 mthdata['tr_empl_gap_perc'] = mthdata['tr_empl_gap']/mthdata['hp_ch'] * 100
                 mthdata['act_infl'] = \
                     mthdata['PCE_seas'].diff(periods=12)/mthdata['PCE_seas']*100
@@ -140,23 +117,23 @@ for source in sources:
                 x_t_na = mod_data.copy()
                 for t in range(k_ar-1):
                     for var in mod_data.columns:
-                        x_t_na[var + '_m' + str(t+1)] = pa.Series(mod_data[var].values[:-(t+1)],
+                        x_t_na[var + '_m' + str(t+1)] = px.Series(mod_data[var].values[:-(t+1)],
                                                             index=mod_data.index[t+1:])
                 #remove missing values
                 x_t = x_t_na.dropna(axis=0)
 
                 if source == 'fbliss':
-                    ycdata = pa.read_csv("./data/fama-bliss_formatted.csv",
+                    ycdata = px.read_csv("./data/fama-bliss_formatted.csv", 
                                          na_values = "M", index_col=0,
                                          parse_dates=True, sep=",")
 
-                    yc_cols = ['TMYTM_1','TMYTM_2','TMYTM_3','TMYTM_5']
+                    yc_cols = ['TMYTM_1','TMYTM_2','TMYTM_3','TMYTM_4','TMYTM_5']
                     mod_yc_data_nodp = ycdata[yc_cols]
                     mod_yc_data_nodp['year'] = mod_yc_data_nodp.index.year
                     mod_yc_data_nodp['month'] = mod_yc_data_nodp.index.month
                     mod_yc_data_nodp['day'] = 1
                     mod_yc_data_nodp['new_dt'] = mod_yc_data_nodp.apply(
-                        lambda row: dt.datetime(int(row['year']),
+                        lambda row: dt.datetime(int(row['year']), 
                                                 int(row['month']),
                                                 int(row['day'])), axis=1)
                     mod_yc_data_nodp.set_index('new_dt', inplace=True)
@@ -166,15 +143,18 @@ for source in sources:
                     mod_yc_data.insert(0, 'trcr_m1', mod_yc_data['fed_funds'])
                     mod_yc_data = mod_yc_data.drop(['fed_funds'], axis=1)
 
-                    mths = [12, 24, 36, 60]
+                    mths = [12, 24, 36, 48, 60]
                     del mod_yc_data['trcr_m1']
                 else:
-                    ycdata = pa.read_csv("./data/yield_curve.csv",
+                    ycdata = px.read_csv("./data/yield_curve.csv", 
                                          na_values = "M", index_col=0,
                                          parse_dates=True, sep=";")
 
-                    mod_yc_data_nodp = ycdata.reindex(columns=['trcr_y1', 'trcr_y2',
-                                                          'trcr_y3', 'trcr_y5'])
+                    mod_yc_data_nodp = ycdata.reindex(columns=['trcr_m3', 
+                                                          'trcr_m6',
+                                                          'trcr_y1', 'trcr_y2',
+                                                          'trcr_y3', 'trcr_y5',
+                                                          'trcr_y7', 'trcr_y10'])
                     mod_yc_data = mod_yc_data_nodp.dropna(axis=0)
                     mod_yc_data = mod_yc_data.join(x_t['fed_funds'], how='right')
                     mod_yc_data.insert(0, 'trcr_m1', mod_yc_data['fed_funds'])
@@ -182,7 +162,7 @@ for source in sources:
 
                     mod_yc_data = to_mth(mod_yc_data)
 
-                    mths = [12, 24, 36, 60]
+                    mths = [3, 6, 12, 24, 36, 60, 84, 120]
                     del mod_yc_data['trcr_m1']
 
                 # Setup model
@@ -195,8 +175,8 @@ for source in sources:
 
                 #subset to range specified in BSR
 
-                var_dates = pa.date_range("5/1/1990", "5/1/2012", freq="MS").to_pydatetime()
-                yc_dates = pa.date_range("8/1/1990", "5/1/2012", freq="MS").to_pydatetime()
+                var_dates = px.date_range("5/1/1990", "5/1/2012", freq="MS").to_pydatetime()
+                yc_dates = px.date_range("8/1/1990", "5/1/2012", freq="MS").to_pydatetime()
 
                 mod_data = mod_data.ix[var_dates]
                 mod_yc_data = mod_yc_data.ix[yc_dates]
@@ -243,8 +223,8 @@ for source in sources:
                                 b_solve, solv_cov = out_bsr
 
                 a_rsk, b_rsk = bsr_model.gen_pred_coef(lam_0=lam_0, lam_1=lam_1,
-                                                    delta_0=delta_0,
-                                                    delta_1=delta_1, mu=mu,
+                                                    delta_0=delta_0, 
+                                                    delta_1=delta_1, mu=mu, 
                                                     phi=phi, sigma=sigma)
 
                 #generate no risk results
@@ -255,26 +235,28 @@ for source in sources:
                                                          lam_1=lam_1_nr,
                                                          delta_0=delta_0,
                                                          delta_1=delta_1, mu=mu,
-                                                         phi=phi,
+                                                         phi=phi, 
                                                          sigma=sigma_zeros)
                 if source == 'fbliss':
                     #gen BSR predicted
                     X_t = bsr_model.var_data_vert
                     per = bsr_model.yc_data.index
-                    act_pred = pa.DataFrame(index=per)
+                    act_pred = px.DataFrame(index=per)
                     for i in mths:
                         act_pred[str(i) + '_mth_act'] = bsr_model.yc_data['TMYTM_'
                                                                       + str(i/12)]
                         act_pred[str(i) + '_mth_pred'] = a_rsk[i-1] + \
-                                                        np.dot(b_rsk[i-1],
+                                                        np.dot(b_rsk[i-1], 
                                                         X_t.values.T)
                         act_pred[str(i) + '_mth_nrsk'] = a_nrsk[i-1] + \
-                                                        np.dot(b_nrsk[i-1].T,
+                                                        np.dot(b_nrsk[i-1].T, 
                                                                X_t.values.T)
                         act_pred[str(i) + '_mth_err'] = np.abs( \
                             act_pred[str(i) + '_mth_act'] - \
-                            act_pred[str(i) + '_mth_pred'])
+                            act_pred[str(i) + '_mth_pred'])**2
                     five_yr = act_pred.reindex(columns = filter(lambda x: '60' in
+                                                                x,act_pred))
+                    four_yr = act_pred.reindex(columns = filter(lambda x: '48' in
                                                                 x,act_pred))
                     three_yr = act_pred.reindex(columns = filter(lambda x: '36' in
                                                                  x,act_pred))
@@ -284,49 +266,51 @@ for source in sources:
                                                                x,act_pred))
 
                     #generate st dev of residuals
-                    yields = ['one_yr', 'two_yr', 'three_yr', 'five_yr']
-                    ynames = ['one', 'two', 'three', 'five']
-                    for yix, yld in enumerate(yields):
-                        df = ynames[yix] + '_year_res_' + source
-                        col = model_names[mix]
-                        print df + col
-                        eval(df)[col] = eval(yld).filter(regex='.*err$')
+                    yields = ['one_yr', 'two_yr', 'three_yr', 'four_yr', 'five_yr']
+                    for yld in yields:
+                        print yld + " & " + str(np.sum(eval(yld).filter(
+                                                regex= '.*err$').values)\
+                                                /len(one_yr))
                 else:
                     #gen BSR predicted
                     X_t = bsr_model.var_data_vert
                     per = bsr_model.yc_data.index
-                    act_pred = pa.DataFrame(index=per)
+                    act_pred = px.DataFrame(index=per)
                     for i in mths:
                         act_pred[str(i) + '_mth_act'] = bsr_model.yc_data[ \
                                 'trcr_m' + str(i)]
                         act_pred[str(i) + '_mth_pred'] = a_rsk[i-1] + \
-                                                        np.dot(b_rsk[i-1],
+                                                        np.dot(b_rsk[i-1], 
                                                         X_t.values.T)
                         act_pred[str(i) + '_mth_nrsk'] = a_nrsk[i-1] + \
-                                                        np.dot(b_nrsk[i-1].T,
+                                                        np.dot(b_nrsk[i-1].T, 
                                                                X_t.values.T)
                         act_pred[str(i) + '_mth_err'] = np.abs( \
                             act_pred[str(i) + '_mth_act'] - \
-                            act_pred[str(i) + '_mth_pred'])
+                            act_pred[str(i) + '_mth_pred'])**2
+                    ten_yr = act_pred.reindex( \
+                        columns = filter(lambda x: '120' in x, act_pred))
+                    seven_yr = act_pred.reindex( \
+                        columns = filter(lambda x: '84' in x, act_pred))
                     five_yr = act_pred.reindex( \
                         columns = filter(lambda x: '60' in x,act_pred))
                     three_yr = act_pred.reindex( \
                         columns = filter(lambda x: '36' in x,act_pred))
                     two_yr = act_pred.reindex( \
                         columns = filter(lambda x: '24' in x,act_pred))
-                    one_yr = act_pred.reindex(columns = ['12_mth_act',
+                    one_yr = act_pred.reindex(columns = ['12_mth_act', 
                                                          '12_mth_pred',
-                                                         '12_mth_nrsk',
+                                                         '12_mth_nrsk', 
                                                          '12_mth_err'])
+                    six_mth = act_pred.reindex(columns = ['6_mth_act', 
+                                                          '6_mth_pred',
+                                                          '6_mth_nrsk', 
+                                                          '6_mth_err'])
 
                     #generate st dev of residuals
-                    yields = ['one_yr', 'two_yr', 'three_yr', 'five_yr']
-                    ynames = ['one', 'two', 'three', 'five']
-                    for yix, yld in enumerate(yields):
-                        df = ynames[yix] + '_year_res_' + source
-                        col = model_names[mix]
-                        print df + col
-                        eval(df)[col] = eval(yld).filter(regex='.*err$')
-
-for df in dfs:
-    eval(df).to_csv('./ss_results/' + df + '.csv', float_format='%.3f')
+                    yields = ['six_mth', 'one_yr', 'two_yr', 'three_yr', 'five_yr',
+                              'seven_yr', 'ten_yr']
+                    for yld in yields:
+                        print yld + " & " + str(np.sum(eval(yld).filter(
+                                                regex= '.*err$').values)\
+                                                /len(one_yr))
