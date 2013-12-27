589595e072fee59239fa1e553bb15a921ec857d0
Bart Baker <bartbkr@gmail.com>
2013-12-27 14:03:15 -0500

    Cleaned up master branch for simpler structure

--- tests/old_runs/uncert_djia_png.py
+++
@@ -1,8 +0,0 @@
-import pandas as px
-mthdata = px.read_csv("./data/macro_data.csv", na_values="M",
-                        index_col = 0, parse_dates=True, sep=";")
-newguy = mthdata[['uncert', 'djialog_std']]
-var_dates = px.date_range("5/1/1990", "5/1/2012", freq="MS").to_pydatetime()
-newguy = newguy.ix[var_dates]
-newguy.plot(secondary_y='djialog_std')
-plt.show()
